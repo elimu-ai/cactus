f290e32f5ce6f0c12d5e49c8b0a6e5055f295c31
HMUNACHI <ndubuakuhenry@gmail.com>
2025-05-02 20:10:59 +0100

    android and cactus refactor work

--- scripts/build-flutter.sh
+++ scripts/build-flutter.sh
@@ -4,16 +4,17 @@ SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ROOT_DIR="$(cd "$SCRIPT_DIR/.." && pwd)"
 
 # Build the Flutter package 
-cd "$ROOT_DIR/flutter" 
+cd "$ROOT_DIR/cactus_flutter" 
 
 echo "Copying iOS frameworks and Android libraries to React Native project..."
 
 # Copy the iOS framework and project files
 cp -R "$ROOT_DIR/ios/cactus.xcframework" ios/ 
-cp -R "$ROOT_DIR/ios/CMakelists.txt" ios/ 
+cp -R "$ROOT_DIR/ios/CactusContext.h" ios/Classes/
+cp -R "$ROOT_DIR/ios/CactusContext.mm" ios/Classes/
 
 # Copy the contents of android/src/main into react/android/src/main
 # This copies all files without replacing the directory (there are important unique files)
 cp -R "$ROOT_DIR/android/src/main"/* android/src/main/
 
-flutter pub get
+# flutter pub get
