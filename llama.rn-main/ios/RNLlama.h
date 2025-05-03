87f687589be7f4add00a633c765ffe7e572a78b5
HMUNACHI <ndubuakuhenry@gmail.com>
2025-05-03 17:24:54 +0100

    cleanup

--- llama.rn-main/ios/RNLlama.h
+++
@@ -1,7 +0,0 @@
-#import <React/RCTEventEmitter.h>
-#import <React/RCTBridgeModule.h>
-
-// TODO: Use RNLlamaSpec (Need to refactor NSDictionary usage)
-@interface RNLlama : RCTEventEmitter <RCTBridgeModule>
-
-@end
